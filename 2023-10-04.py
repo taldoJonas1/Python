d934b3539e03ca120a74f5cb47b7091a7fbf8698
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2023-10-06 20:31:22 -0300

    Update 2023-10-04.py

--- 2023-10-04.py
+++ 2023-10-04.py
@@ -58,3 +58,39 @@ while nro != 0:
         cont += 1
 media = soma / cont
 print(f'A média dos nros pares inseridos foi {media}')      
+
+=======================================================================================================================
+
+'''
+Imagine uma brincadeira entre dois colegas, na qual um pensa um número 
+e o outro deve fazer chutes até acertar o número imaginado. Como dica, 
+a cada tentativa é dito se o chute foi alto ou foi baixo. Elabore um 
+programa dentro deste contexto, que leia o número imaginado e os chutes, 
+ao final mostre quantas tentativas foram necessárias para descobrir o número.
+'''
+
+tent = 0
+import random
+n1 = random.randint(0,50)
+n2 = int(input('Tente acertar o número: '))
+
+while n2 != n1:
+    if n2 > n1:
+        n2 = int(input('Menos\n'))
+        tent += 1
+    elif n2 < n1:
+        n2 = int(input('Mais\n'))
+        tent += 1
+
+if n2 == n1:
+    print(f'ACERTOU!!!\n{tent + 1} tentativas.')
+
+=======================================================================================================================
+
+'''
+Faça um programa que permita fazer um levantamento do estoque de vinhos de 
+uma adega, tendo como dados de entrada tipos de vinho, sendo: 'T' para tinto, 
+'B' para branco e 'R' para rosê. Especifique a porcentagem de cada tipo sobre 
+o total geral de vinhos; a quantidade de vinhos é desconhecida, utilize para 
+finalizador do algoritmo a opção 'F' de fim.
+'''
